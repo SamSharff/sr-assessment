364c7855d5ed603d7688ccf81d1e3cc127f807fa
Sam Sharff <samanthaleec@gmail.com>
2022-10-28 10:10:18 -0400

    adds cards object; creates remaind cards/drawn cards tables; loops through test data and displays on page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,85 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      cards: [
+        {
+          id: 0,
+          suit: "spades",
+          value: 0,
+        },
+        {
+          id: 1,
+          suit: "clubs",
+          value: 0,
+        },
+        {
+          id: 2,
+          suit: "diamonds",
+          value: 0,
+        },
+        {
+          id: 3,
+          suit: "hearts",
+          value: 0,
+        },
+        {
+          id: 4,
+          suit: "spades",
+          value: 1,
+        },
+      ],
+    };
   },
+  created: function () {},
+  methods: {},
 };
 </script>
+
+<template>
+  <div class="home">
+    <div class="remaining-cards">
+      <!-- REMAINING CARDS TABLE -->
+      <h3>Remaining Cards</h3>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Suit</th>
+            <th scope="col">Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="card in cards" :key="card.id">
+            <td>{{ card.suit }}</td>
+            <td>{{ card.value }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="drawn-cards">
+      <!-- Drawn CARDS TABLE -->
+      <h3>Drawn Cards</h3>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Suit</th>
+            <th scope="col">Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="card in cards" :key="card.id">
+            <td>{{ card.suit }}</td>
+            <td>{{ card.value }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<style>
+table,
+th,
+td {
+  border: 1px solid black;
+}
+</style>
